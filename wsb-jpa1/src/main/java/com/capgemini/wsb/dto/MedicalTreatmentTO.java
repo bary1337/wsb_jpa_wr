7a4f599745728a746c6f8e859b3746e87eec7bb6
Wojciech Rościszewski <wojciech.rosciszewski00@gmail.com>
2024-05-18 21:35:46 +0200

    Stworzenie DoctorTO, MedicalTreatmentTO, PatientTO, VisitTO.

---
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/dto/MedicalTreatmentTO.java
@@ -0,0 +1,40 @@
+package com.capgemini.wsb.dto;
+
+import com.capgemini.wsb.persistence.enums.TreatmentType;
+import java.util.List;
+public class MedicalTreatmentTO {
+    private Long id;
+    private String description;
+    private TreatmentType type;
+    private List<MedicalTreatmentTO> treatments;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TreatmentType getType() {
+        return type;
+    }
+
+    public void setType(TreatmentType type) {
+        this.type = type;
+    }
+    public List<MedicalTreatmentTO> getTreatments() {
+        return treatments;
+    }
+
+    public void setTreatments(List<MedicalTreatmentTO> treatments) {
+        this.treatments = treatments;
+    }
+}
---
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/dto/PatientTO.java
@@ -0,0 +1,103 @@
+package com.capgemini.wsb.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import com.capgemini.wsb.persistence.entity.VisitEntity;
+import java.util.ArrayList;
+import java.util.List;
+public class PatientTO {
+    public PatientTO(){}
+    public PatientTO(Long id, String firstName, String lastName, String telephoneNumber, String email, String patientNumber, LocalDate dataOfBirth, AddressTO address, List<VisitTO> visits)
+    {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.telephoneNumber = telephoneNumber;
+        this.email = email;
+        this.patientNumber = patientNumber;
+        this.dataOfBirth = dataOfBirth;
+        this.address = address;
+        this.visits = visits;
+    }
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String telephoneNumber;
+    private String email;
+    private String patientNumber;
+    private LocalDate dataOfBirth;
+    private AddressTO address;
+    private List<VisitTO> visits;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPatientNumber() {
+        return patientNumber;
+    }
+
+    public void setPatientNumber(String patientNumber) {
+        this.patientNumber = patientNumber;
+    }
+
+    public LocalDate getDataOfBirth() {
+        return dataOfBirth;
+    }
+
+    public void setDataOfBirth(LocalDate dataOfBirth) {
+        this.dataOfBirth = dataOfBirth;
+    }
+
+    public AddressTO getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressTO address) {
+        this.address = address;
+    }
+
+    public List<VisitTO> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(List<VisitTO> visits) {
+        this.visits = visits;
+    }
+}
